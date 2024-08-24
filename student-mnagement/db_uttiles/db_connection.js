ddcbafd398af42894cc7e5264ec455f6d2bee1cc
bijas <bijas.backer.et@gmail.com>
2024-08-25 01:55:28 +0530

    f

--- student-mnagement/db_uttiles/db_connection.js
+++ student-mnagement/db_uttiles/db_connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 
 const connection = mysql.createConnection({
-    host: process.env.HOST,
+    host: "sql12.freemysqlhosting.net",
     user: 'sql12727658' ,    //process.env.USER,
     password:  '65vbDUhYCF',   // process.env.PASSWORD, 
     database: 'sql12727658'   //process.env.DATABASE
