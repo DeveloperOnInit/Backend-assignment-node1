d83db18658ceab5092fe6cfce2182cdbd8cee5a0
bijas <bijas.backer.et@gmail.com>
2024-08-25 14:31:09 +0530

    test

--- student-mnagement/api/app.js
+++ student-mnagement/api/app.js
@@ -10,7 +10,7 @@ app.use("/api/school", studentRoutes);
 
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
